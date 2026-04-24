e557f7f63be5c5ab59fa156f4226700fa07b5cbd
HEYU CHONG <heychong@c3r3s7.42tokyo.jp>
2026-04-24 13:46:09 +0900

    utils.py: [error handling] cases where brackets aren't closed  [error handling] when extra information appears after the brackets

--- utils.py
+++ utils.py
@@ -6,11 +6,14 @@ def detect_bracket(string: str) -> Optional[str]:
     start = string.find("[")
     end = string.find("]")
 
-    if start == -1 or end == -1:
+    if start == -1 and end == -1:
         return None
     if end < start:
-        raise ValueError(f"Malformed brackets in: '{string}'")
+        raise ValueError(f"malformed brackets in: '{string}'")
     if string.count("[") > 1 or string.count("]") > 1:
-        raise ValueError(f"Multiple brackets in: '{string}")
+        raise ValueError(f"multiple brackets in: '{string}")
+    if string[end] != string[-1]:
+        raise ValueError("extra string detected after brackets"
+                         + f" - '... {string[end:]}'")
 
     return string[start: end + 1]
